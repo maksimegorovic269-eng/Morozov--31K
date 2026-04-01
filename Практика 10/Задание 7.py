671f725a6867adbd6446b28a94284e8b4403c927
maksimegorovic269-eng <maksimegorovic269@gmail.com>
2026-04-01 14:26:15 +0500

    Add files via upload

---
+++ Практика 10/Задание 7.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+    
+    def __str__(self):
+        return f"Person: {self.name}"
+
+# Пример использования
+p = Person("Alice")
+print(p)
---
+++ Практика 8/Задание 7.py
@@ -0,0 +1,15 @@
+class Calculator:
+    def calculate(self, x, y):
+        return x + y
+
+class SafeCalculator(Calculator):
+    def calculate(self, x, y):
+        if isinstance(x, (int, float)) and isinstance(y, (int, float)):
+            return x + y
+        else:
+            return "Error"
+
+# Пример использования
+calc = SafeCalculator()
+print(calc.calculate(2, 3))
+print(calc.calculate(2, "a"))
